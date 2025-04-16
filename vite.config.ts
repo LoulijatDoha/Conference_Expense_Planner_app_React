4367252710d6ef0dc448c82e9ae8fb90f00f1764
DohaLoulijat <dohaloulijat19@gmail.com>
2025-04-16 17:32:56 -0400

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/Conference_Expense_Planner_app_React/",
   plugins: [react()],
 })
